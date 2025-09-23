772fab6a2d22449466387391e7631857eaa1acc0
LearnwithPeter <learnwithpeter543@gmail.com>
2025-09-23 10:05:00 +0530

    06CurrencyConverter_Project

---
+++ 06curruncyConverter/src/hooks/usecurrencyinfo.js
@@ -0,0 +1,15 @@
+import { useEffect,useState } from "react";
+
+function usecurrencyinfo(currency){
+  const [data,setdata]=useState({})
+  useEffect(()=>{
+    fetch(`https://v6.exchangerate-api.com/v6/5c45cf67d4a3774c9754ade7/latest/${currency}`)
+    .then((res)=>res.json)
+    .then((res)=> res[currency])
+    console.log(data);
+
+  },[currency])
+  console.log(data);
+  return data 
+}
+export default usecurrencyinfo;
