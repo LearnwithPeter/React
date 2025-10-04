6730636afea10e23bd8ad4217560549a2f8b8b21
LearnwithPeter <learnwithpeter543@gmail.com>
2025-10-04 16:02:57 +0530

    Theme Switcher

---
+++ 09Themeswitcher/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { Themeprovider } from './contexts/theme.js'
+import Themebtn from './components/Themebtn'
+import Card from './components/Card'
+
+function App() {
+  const [themeMode,setthemeMode]=useState("light") 
+ 
+  const lightTheme=()=>{
+    setthemeMode("light")
+  }
+
+  const darkTheme=()=>{
+    setthemeMode("dark")
+  }
+//actual change in theme 
+useEffect(() => {
+ document.querySelector('html').classList.remove("light","dark")
+ document.querySelector('html').classList.add(themeMode)
+
+}, [themeMode])
+
+  return (
+    <Themeprovider value={{themeMode,lightTheme,darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Themebtn/>
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
+        </div>
+      </div>
+    </Themeprovider>
+   )
+}
+
+export default App
